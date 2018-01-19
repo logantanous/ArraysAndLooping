18ba63d4d30502954ea62b20af9dba7002e9ad3e
logan tanous1 <logantanous1@Logans-MBP.fios-router.home>
2018-01-19 10:22:08 -0800

    add logic

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+//back end
+function processNumber(input) {
+  if (input % 3 === 0) {
+    return "I'm sorry, Dave. I'm afraid I can't do that.";
+  } else {
+    if (String(input).match(/1/)) {
+      return "Boop!";
+    } else {
+      if (String(input).match(/0/)) {
+        return "Beep!";
+      } else {
+        return input;
+      }
+    }
+  }
+}
+
+function robotify(input) {
+  for (var i = 0; i <= input; i++) {
+    var result = processNumber(i);
+    $(".result").append(result+"<br>");
+  }
+}
+
+//front end
+$(document).ready(function() {
+  $("#robotifyButton").click(function() {
+    $(".result").html('');
+    var regularText = $("input").val();
+      robotify(regularText);
+  })
+});
